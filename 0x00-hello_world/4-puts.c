cd2dacc518bf032e83792e52f75a7e600851e4b3
Busi <mqulwanabucie@gmail.com>
2023-06-15 04:28:42 -0700

    0x00. C - Hello, World

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+/* main is the function body */
+int main(void) {
+/* function puts prints a message and appends a new line */
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+/* main is the function body */
+int main(void) {
+/* function prinf prints a message */
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
