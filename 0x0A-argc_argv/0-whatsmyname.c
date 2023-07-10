362058e012e132a817a03ffd276bc3fa9457872e
Busi <mqulwanabucie@gmail.com>
2023-07-10 06:43:18 -0700

    0x0A. C - argc, argv

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - prints its name
+ * @argc: argument count
+ * @argv: argument array
+ * Return: it's name
+ */
+
+int main(int argc, char *argv[])
+{
+(void) argc;
+printf("%s\n", argv[0]);
+return (0);
+}
+
---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,68 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * calculateCoins - calculates the minimum number of coins
+ * @cents: the amount in cents
+ * Return: number of coins
+ */
+int calculateCoins(int cents)
+{
+int coins = 0;
+
+while (cents > 0)
+{
+if (cents >= 25)
+{
+cents -= 25;
+}
+else if (cents >= 10)
+{
+cents -= 10;
+}
+else if (cents >= 5)
+{
+cents -= 5;
+}
+else if (cents >= 2)
+{
+cents -= 2;
+}
+else
+{
+cents -= 1;
+}
+coins++;
+}
+return (coins);
+}
+
+/**
+ * main - prints its name
+ * @argc: argument count
+ * @argv: argument array
+ * Return: it's name
+ */
+
+int main(int argc, char *argv[])
+{
+int cents, coins;
+
+if (argc != 2)
+{
+printf("Error\n");
+return (1);
+}
+cents = atoi(argv[1]);
+
+if (cents < 0)
+{
+printf("0\n");
+return (0);
+}
+coins = calculateCoins(cents);
+
+printf("%d\n", coins);
+return (0);
+}
+
