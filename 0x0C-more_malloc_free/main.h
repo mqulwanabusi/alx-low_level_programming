daf1753577812b9e0d31fd1f85c007b862e81b5f
Busi <mqulwanabucie@gmail.com>
2023-07-13 04:57:10 -0700

    More malloc, free

---
+++ 0x0C-more_malloc_free/main.h
@@ -0,0 +1,13 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+extern int _putchar(char c);
+extern char *create_array(unsigned int size, char c);
+extern char *str_concat(char *s1, char *s2);
+extern int **alloc_grid(int width, int height);
+extern void free_grid(int **grid, int height);
+extern char **strtow(char *str);
+extern char *argstostr(int ac, char **av);
+
+#endif /* MAIN_H */
+
